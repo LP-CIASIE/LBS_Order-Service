a8e9da9f6e7700290897260ad200b9f1fb728d0f
Teddy CLEMENT DELS <teddy.clement_dels@outlook.fr>
2023-03-13 10:43:45 +0100

    TP4_part2 fini (#3)

--- src/actions/OrderByIdAction.php
+++ src/actions/OrderByIdAction.php
@@ -10,6 +10,8 @@ use lbs\order\services\OrderServices;
 use lbs\order\errors\exceptions\RessourceNotFoundException;
 use Slim\Exception\HttpNotFoundException;
 
+use Slim\Routing\RouteContext;
+
 use \lbs\order\services\utils\FormatterAPI;
 
 final class OrderByIdAction
@@ -31,9 +33,22 @@ final class OrderByIdAction
     } catch (RessourceNotFoundException $e) {
       throw new HttpNotFoundException($rq, $e->getMessage());
     }
+
+    $rs->getBody()->write("orders, {$args['id']}");
+
+    $routeParser = RouteContext::fromRequest($rq)->getRouteParser();
+
     $data = [
       'type' => 'resource',
-      'order' => $order
+      'order' => $order,
+      'links' => [
+        'items' => [
+          'href' => $routeParser->urlFor('ordersItems', ['id' => $args['id']])
+        ],
+        'self' => [
+          'href' => $routeParser->urlFor('ordersById', ['id' => $args['id']] ,)
+        ],
+      ] 
     ];
 
     return FormatterAPI::formatResponse($rq, $rs, $data);
