921dde7917dc7166a80883d728e76aaca50f2dee
lecyp12 <contact@cyprien-cotinaut.com>
2023-03-13 10:22:34 +0100

    ajout pagination et verification dernière page dans action

--- src/actions/OrdersAction.php
+++ src/actions/OrdersAction.php
@@ -5,6 +5,9 @@ namespace lbs\order\actions;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+use lbs\order\errors\exceptions\RessourceNotFoundException;
+use Slim\Exception\HttpNotFoundException;
+
 use lbs\order\services\OrderServices;
 use \lbs\order\services\utils\FormatterAPI;
 
@@ -14,12 +17,41 @@ final class OrdersAction
     Request $rq,
     Response $rs
   ): Response {
+    $query = $rq->getQueryParams();
     $os = new OrderServices();
-    $orders = $os->getOrders();
 
+    $page = 0;
+    $sizePage = 100;
+
+    if (isset($query['page']) && $query['page'] > 0) {
+      $page = $query['page'];
+    }
+
+    // if (isset($query['size']) && $query['size'] > 0) {
+    //   $sizePage = $query['size'];
+    // }
+
+    try {
+
+      $countOrders = $os->getCountOrders();
+
+      $lastPage = ceil($countOrders / $sizePage) - 1;
+
+      if ($page > $lastPage) {
+        $page = $lastPage;
+      }
+
+
+      $orders = $os->getOrders($page, $sizePage);
+    } catch (RessourceNotFoundException $e) {
+      throw new HttpNotFoundException($rq, $e->getMessage());
+    }
     $data = [
       'type' => 'collection',
-      'count' => count($orders),
+      'count' => $countOrders,
+      'countB' => count($orders),
+      'last-page' => $lastPage,
+      'size' => $sizePage,
       'orders' => $orders
     ];
 
